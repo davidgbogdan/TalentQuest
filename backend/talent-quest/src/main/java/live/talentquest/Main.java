3dce98fa9efb39d90f613c12248aef7c069a4895
david <davidgeorgebogdan@gmail.com>
2024-06-18 06:08:54 +0300

    update docker-compose.yaml

--- backend/talent-quest/src/main/java/live/talentquest/Main.java
+++ backend/talent-quest/src/main/java/live/talentquest/Main.java
@@ -1,9 +1,11 @@
 package live.talentquest;
 
+import lombok.AllArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@AllArgsConstructor
 public class Main {
 
 	public static void main(String[] args) {
