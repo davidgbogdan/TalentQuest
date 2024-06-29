732a9f9b8a553d07cccdc25a65fc1f85d4ce0208
david <davidgeorgebogdan@gmail.com>
2024-06-30 02:38:11 +0300

    fixing navbar to appear only once the user is logged in

--- talentquest-frontend/src/components/Layout.js
+++ talentquest-frontend/src/components/Layout.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import NavBar from './NavBar';
 
 const Layout = () => {
+  const location = useLocation();
+  const noNavBarRoutes = ['/','/register'];
+
   return (
     <>
-      <NavBar />
+      {!noNavBarRoutes.includes(location.pathname) && <NavBar />}
       <Outlet />
     </>
   );
