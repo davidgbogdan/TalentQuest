e6fcb1e50a9eab0abbfe7181abda879aa91f01b8
david <davidgeorgebogdan@gmail.com>
2024-08-25 22:46:07 +0300

    Remove talentquest-frontend directory

--- talentquest-frontend/src/services/candidateService.js
+++
@@ -1,9 +0,0 @@
-import api from './api';
-
-export const register = (candidateData) => {
-  return api.post('/candidates', candidateData);
-};
-
-export const login = (userSessionDto) => {
-  return api.post('/candidates/sessions', userSessionDto);
-};
